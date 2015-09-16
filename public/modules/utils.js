fb97a237f3801373dfea3cd2a357b628f9d61d85
朱磊 <lei.zhu@tcl.com>
2015-09-16 17:35:08 +0800

    bind data to the template

--- public/modules/utils.js
+++ public/modules/utils.js
@@ -72,7 +72,7 @@ define(function(require, exports, module) {
 	 * @return {[type]}      [解析后的字符串]
 	 */
 	function formatJson(str, data) {
-		var fn = !/\W/.test(str) ? _formatJson_cache[str] = _formatJson_cache[str] || $formatJson(document.getElementById(str).innerHTML) : new Function("obj", "var p=[],print=function(){p.push.apply(p,arguments);};" + "with(obj){p.push('" + str.replace(/[\r\t\n]/g, " ").split("<$").join("\t").replace(/((^|$>)[^\t]*)'/g, "$1\r").replace(/\t=(.*?)$>/g, "',$1,'").split("\t").join("');").split("$>").join("p.push('").split("\r").join("\\'") + "');}return p.join('');");
+		var fn = !/\W/.test(str) ? _formatJson_cache[str] = _formatJson_cache[str] || $formatJson(document.getElementById(str).innerHTML) : new Function("obj", "var p=[],print=function(){p.push.apply(p,arguments);};" + "with(obj){p.push('" + str.replace(/[\r\t\n]/g, " ").split("<$").join("\t").replace(/((^|$>)[^\t]*)'/g, "$1\r").replace(/\t=(.*?)\$>/g, "',$1,'").split("\t").join("');").split("$>").join("p.push('").split("\r").join("\\'") + "');}return p.join('');");
 		return data ? fn(data) : fn;
 	}
 
