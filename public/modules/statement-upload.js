b3d9627d9d6da701f3eb7614e0303ede98084c98
xueyan.hu <xueyan.hu@tcl.com>
2015-09-30 12:09:39 +0800

    上传

--- public/modules/statement-upload.js
+++ public/modules/statement-upload.js
@@ -1,13 +1,27 @@
 define(function(require, exports, module) {
+    var art_dialog = require('dialog');
 	function init() {
-        $('#uploadFile').fileupload({
-            url: global_config.serverRoot + "/reconciliation",
-            done: function (e, data) {
-                console.log(data);
-            }
-        })
+        uploadfile();
 	}
 
+    function uploadfile() {
+        $('#uploadFile').fileupload({
+            url: global_config.serverRoot + "/reconciliation",
+            start: function () {
+                art_dialog.loading.start("uploading");
+            },
+            done: function (e, data) {
+                console.log(data);
+            },
+            always:function(e, data){
+                art_dialog.loading.end();
+                if(data.result && data.result.code != 0){
+                    dialog.error('Failed', data.result.msg);
+                }
+
+            }
+        })
+    }
 
 	return {
 		init: init
