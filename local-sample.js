e640be1a010cd57dd9694e126547fdcfe19535b2
hangyang.wang <w.why@163.com>
2015-10-14 15:35:22 +0800

    settlementWeb

--- local-sample.js
+++ local-sample.js
@@ -1,7 +1,7 @@
 module.exports = {
 	port: '3000', //监听的端口号
-	proxyPath: '/qf', //url根路径
-	filePath: '/qf'//引用静态前端文件(js,css)路径
+	proxyPath: '/settlementWeb', //url根路径
+	filePath: '/settlementWeb'//引用静态前端文件(js,css)路径
 
 /*说明
 不通过代理访问 或者 代理的路径为根目录时请如下配置
